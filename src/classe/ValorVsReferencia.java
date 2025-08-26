f94cd259725f51d4c57b30bedbde14c93571cdf2
felipemaraboti <felipemarabotti989@gmail.com>
2025-08-26 14:42:01 -0300

    Avanço de atribuição por valor e referencia

---
+++ src/classe/ValorVsReferencia.java
@@ -0,0 +1,33 @@
+package classe;
+
+public class ValorVsReferencia {
+    public static void main(String[] args) {
+        double a = 2;
+        double b = a; //Atribuição por valor (Primitivos)
+
+        a++;
+        b--;
+
+        System.out.println(a + " " + b);
+
+        Data d1 = new Data(1, 6, 2020);
+        Data d2 = d1; //Atribuição por referência (Objeto)
+
+        d1.dia = 12;
+        d2.mes = 4;
+
+        System.out.println(d1.obterDataFormatada());
+        System.out.println(d2.obterDataFormatada());
+
+        voltarDataParaValorPadrao(d1);
+
+        System.out.println(d1.obterDataFormatada());
+        System.out.println(d2.obterDataFormatada());
+    }
+
+    static void voltarDataParaValorPadrao(Data d) {
+        d.dia = 1;
+        d.mes = 1;
+        d.ano = 1970;
+    }
+}
